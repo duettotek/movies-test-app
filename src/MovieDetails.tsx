1a672621dca7233bba401de5a12116bc49171718
duettotek <duettotek@live.it>
2021-09-11 16:30:43 +0200

    first poor layout

--- src/MovieDetails.tsx
+++ src/MovieDetails.tsx
@@ -1,31 +1,34 @@
-import React from 'react';
-import { MovieDetails_movie } from './__generated__/MovieDetails_movie.graphql';
-import { createFragmentContainer } from 'react-relay';
-import { graphql } from 'babel-plugin-relay/macro';
+import React from "react";
+import { MovieDetails_movie } from "./__generated__/MovieDetails_movie.graphql";
+import { createFragmentContainer } from "react-relay";
+import { graphql } from "babel-plugin-relay/macro";
+import MovieReviewCard from "./components/MovieReviewCard";
 
 interface Props {
   movie: MovieDetails_movie | null;
 }
 
 function MovieDetails({ movie }: Props) {
-  if(!movie) {
-    return <div></div>
+  if (!movie) {
+    return <div></div>;
   }
   const { id, title, plot } = movie;
-  return (
-    <div key={id}>{`${title} - ${plot}`}</div>
-  );
+  return <MovieReviewCard movie={movie}></MovieReviewCard>;
+  //<div key={id}>{`${title} - ${plot}`}</div>
 }
 
-export default createFragmentContainer(
-    MovieDetails,
-  {
-    movie: graphql`
-      fragment MovieDetails_movie on Movie {
-        id
-        title
-        plot
-      }
-    `
-  }
-);
+export default createFragmentContainer(MovieDetails, {
+  movie: graphql`
+    fragment MovieDetails_movie on Movie {
+      id
+      title
+      plot
+      year
+      runtime
+      genres
+      director
+      actors
+      posterUrl
+    }
+  `,
+});
